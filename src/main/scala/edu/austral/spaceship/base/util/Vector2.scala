dd3b72e1491c45c19f11b5bc4713d0c2d8c7ef78
MaxiAdaro <maximiliano.adaro@ing.austral.edu.ar>
2020-11-07 00:28:51 -0300

    added Vector2

---
+++ src/main/scala/edu/austral/spaceship/base/util/Vector2.scala
@@ -0,0 +1,21 @@
+package edu.austral.spaceship.base.util
+
+import java.lang.Math.{atan2, cos, sin}
+
+case class Vector2(x: Float, y: Float) {
+
+  def +(other: Vector2): Vector2 = Vector2(x + other.x, y + other.y)
+
+  def -(other: Vector2): Vector2 = Vector2(x - other.x, y - other.y)
+
+  def *(scalar: Float): Vector2 = Vector2(x * scalar, y * scalar)
+
+  def rotate(angle: Float): Vector2 = Vector2((x * cos(angle) - y * sin(angle)) toFloat, (x * sin(angle) + y * cos(angle)) toFloat)
+
+  def module: Float = Math.pow(Math.pow(x, 2) + Math.pow(y, 2), 0.5) toFloat
+
+  def unitary: Vector2 = Vector2(x / module, y / module)
+
+  def angle: Float = (atan2(y, x) - atan2(0, 1)) toFloat
+
+}
